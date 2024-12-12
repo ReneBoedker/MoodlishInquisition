325afeeca8a9e78a70f16942ccc64d682c7a3b8f
René B. Christensen <rene@math.aau.dk>
2024-12-12 10:01:51 +0100

    Add common functions for uniform randomness

---
+++ unif/unif.go
@@ -0,0 +1,33 @@
+package unif
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// IntInInterval generates a uniformly random integer in [a,b].
+// If a>b, the function panics
+func IntInInterval(a, b int, allowZero bool) int {
+	if a > b {
+		panic(fmt.Errorf("Cannot generate integer in empty interval [%d, %d]", a, b))
+	}
+	for {
+		tmp := a + rand.Intn(b-a+1)
+		if allowZero || tmp != 0 {
+			return tmp
+		}
+	}
+}
+
+// BoundedInt generates a uniformly random integer in [-a,a].
+func BoundedInt(a int, allowZero bool) int {
+	if a < 0 {
+		return BoundedInt(-a, allowZero)
+	}
+	return IntInInterval(-a, a, allowZero)
+}
+
+// Bool generates a uniformly random boolean value.
+func Bool() bool {
+	return rand.Uint32()&1 == 1
+}
