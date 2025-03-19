6b6c91f1bb728b60c7ae6e760869f414d23ed204
René B. Christensen <rene@math.aau.dk>
2025-03-19 14:41:48 +0100

    Add tests for binary image files

--- graphics/binary.go
+++ graphics/binary.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
+	"net/http"
 	"path/filepath"
 	"strings"
 )
@@ -11,6 +12,7 @@ import (
 type binaryImage struct {
 	content   []byte
 	extension string
+	alt       string
 }
 
 var _ Image = (*binaryImage)(nil) // Ensure that interface is satisfied
@@ -30,14 +32,28 @@ func ImageFromFile(path string) (*binaryImage, error) {
 
 // ImageFromBytes creates an image object directly from given byte slice.
 // The slice should contain the same content as a file on disk of the specified
-// file type.
-// Note that the function will not perform any checks to ensure that the
-// encoding is valid.
-func ImageFromBytes(b []byte, filetype string) *binaryImage {
+// file type. A simple sanity-check is performed, and an error is returned if
+// the check fails. However, an image object with the specified content is
+// produced regardless of the error value.
+func ImageFromBytes(b []byte, filetype string) (*binaryImage, error) {
+	var err error
+	mime := http.DetectContentType(b)
+	if !strings.HasPrefix(mime, "image/") {
+		err = fmt.Errorf("Given bytes do not seem to be an image (detected MIME-type is %q)", mime)
+	} else if ext := strings.TrimPrefix(mime, "image/"); ext != filetype {
+		err = fmt.Errorf("Warning: File seems to be %s, not %s", ext, filetype)
+	}
+
 	return &binaryImage{
 		content:   b,
 		extension: strings.TrimPrefix(filetype, "."),
-	}
+	}, err
+}
+
+// SetAltDescription allows given string to be used as the 'alt' attribute when
+// converting to HTML.
+func (img *binaryImage) SetAltDescription(s string) {
+	img.alt = s
 }
 
 // Filetype returns the filetype of img.
@@ -49,9 +65,15 @@ func (img *binaryImage) Filetype() string {
 // This should be used with care, especially with large image files, as they
 // will be included directly in the HTML code.
 func (img *binaryImage) ToHtml(w io.Writer) {
-	fmt.Fprintf(w, `<img src="data:image/%s;base64,"`, img.Filetype())
+	fmt.Fprintf(w, `<img src="data:image/%s;base64,`, img.Filetype())
+
 	img.ToBase64(w)
-	fmt.Fprintf(w, `">"`)
+
+	if img.alt != "" {
+		fmt.Fprintf(w, `" alt="%s" />`, img.alt)
+	} else {
+		fmt.Fprintf(w, `" />`)
+	}
 }
 
 // ToBase64 encodes img to base64 format.
---
+++ graphics/binary_test.go
@@ -0,0 +1,49 @@
+package graphics
+
+import (
+	"bytes"
+	_ "embed"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"testing"
+)
+
+//go:embed everest.png
+var pngExample []byte
+
+//go:embed everest.jpeg
+var jpegExample []byte
+
+func TestBinaryInputs(t *testing.T) {
+	tests := []*binaryImage{
+		{content: pngExample, extension: "png"},
+		{content: jpegExample, extension: "jpeg"},
+	}
+
+	for i, v := range tests {
+		fmt.Println(v.extension)
+		img, err := ImageFromBytes(v.content, v.extension)
+		if err != nil {
+			t.Fatalf("Creating image produced error: %s", err)
+		}
+
+		if i == 0 {
+			img.SetAltDescription("This is a test")
+		}
+
+		var b bytes.Buffer
+		img.ToHtml(&b)
+
+		d := xml.NewDecoder(&b)
+		for {
+			err := d.Decode(new(any))
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				t.Fatalf("Decoding XML output produced error: %s", err)
+			}
+		}
+	}
+}
